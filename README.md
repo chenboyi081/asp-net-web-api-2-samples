5146e05dc8debe5fcda27298d2f75726ea4684cb
cboii <chenboyi081@163.com>
2015-12-08 19:01:53 +0800

    update md2和添加注释

--- README.md
+++ README.md
@@ -3,15 +3,13 @@
 
 ##第3章
 <p>S301 HttpServer对消息处理管道的构建</p>
-> 自定义MessageHandler
- 
-	public class FooHandler : DelegatingHandler{}
-
-    public class BarHandler : DelegatingHandler{}
-
-    public class BazHandler : DelegatingHandler{}
-
+> 该示例，自定义MessageHandler，利用HttpConfiguration注册自定义的DelegatingHandler，比较创建自定义的HttpServer和初始化后的HttpServer的整个消息处理管道的结构。
+> ![](http://images.cnblogs.com/cnblogs_com/chenboyi081/764976/o_QQ%E6%88%AA%E5%9B%BE20151208183522.png)
 <p>S302 匿名Principal的设置</p>
+> Principal可以简单看成是身份（Identity）与权限（Permission）的封装，它是当前现成安全上下文的一部分。对于HttpServer来说，当它的SendAsync方法被执行的时候，如果当前现成的CurrentPrincipal属性表示的Principal不存在，它会为止创建一个代表“匿名身份的Principal”。
+该示例在两种情况下调用调用自定义的HttpServer对象的SendAsync方法。
+第一次调用发生在当前当前线程的Principal为Null的情况下,第二次调用之前创建了一个用户名为“Artech”的CenericPrincipal对象作为当前线程的Principal。在这两次sendAsync方法调用之后,我们获取当前线程 的 Principal并将其转换成CenericPrincipal对象。
+> ![S302图片](http://images.cnblogs.com/cnblogs_com/chenboyi081/764976/o_2.png)
 <p>S303 验证HttpRoutingDispatcher的路由功能</p>
 <p>S304 验证HttpControllerHandler的路由功能</p>
 <p>S305 自定义HttpMessageHandler实现HTTP方法重写</p>
--- 实例/Chapter 03/S301/WebApi/Controllers/DemoController.cs
+++ 实例/Chapter 03/S301/WebApi/Controllers/DemoController.cs
@@ -12,16 +12,22 @@ namespace WebApi.Controllers
         public Tuple<IEnumerable<string>, IEnumerable<string>> Get()
         {
             HttpConfiguration configuration = new HttpConfiguration();
-            configuration.MessageHandlers.Add(new FooHandler());
+            configuration.MessageHandlers.Add(new FooHandler());          //利用HttpConfiguration注册3个自定义的DelegatingHandler
             configuration.MessageHandlers.Add(new BarHandler());
             configuration.MessageHandlers.Add(new BazHandler());
 
-            MyHttpServer httpServer = new MyHttpServer(configuration);
+            MyHttpServer httpServer = new MyHttpServer(configuration);       //创建自定义的HttpServer
             IEnumerable<string> chain1 = this.GetHandlerChain(httpServer).ToArray();
-            httpServer.Initialize();
+            httpServer.Initialize();                             //调用HttpServer的Initialize方法，从而初始化整个消息处理管道   
             IEnumerable<string> chain2 = this.GetHandlerChain(httpServer).ToArray();
-            return new Tuple<IEnumerable<string>, IEnumerable<string>>(chain1, chain2);
+            return new Tuple<IEnumerable<string>, IEnumerable<string>>(chain1, chain2);     //两个字符串集合组成一个Tuple<IEnumerable<string>, IEnumerable<string>>对象并将其作为Get方法的返回值，这个Tuple反映了HttpMessageHandler链在HttpServer初始化前后的结构
         }
+
+        /// <summary>
+        /// 获得MyHttpServer牵头的HttpMessageHandler链
+        /// </summary>
+        /// <param name="handler"></param>
+        /// <returns></returns>
         private IEnumerable<string> GetHandlerChain(DelegatingHandler handler)
         {
             yield return handler.GetType().Name;
